32722d1c70df51b43ea75259ed00ed1ce58b05d4
punctual-enhancer-1584 <amal.jasen@gmail.com>
2019-09-21 18:05:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  row=" "*3 + "|" +  " "*3 + "|" + " " *3
+  divider= "-" * 11
+  
+  puts row
+  puts divider
+  puts row
+  puts divider
+  puts row
+  
+end
